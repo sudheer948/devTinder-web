bb0959cf361c12b7f39723c30f9614cda3e72d3a
Muhammed Sudheer A.S <muhammedsudheer948@gmail.com>
2026-07-27 12:54:43 +0530

    feat: built connection page

---
+++ src/components/Connections.jsx
@@ -0,0 +1,70 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addConnections } from "../utils/connectionSlice";
+
+const Connections = () => {
+  const connections = useSelector((store) => store.connections);
+  const dispatch = useDispatch();
+
+  const fetchConnections = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/connections", {
+        withCredentials: true,
+      });
+      console.log(res.data.data);
+      dispatch(addConnections(res.data.data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchConnections();
+  }, []);
+
+  if (!connections) return;
+
+  if (connections.length === 0) return <h1>No Connections Found</h1>;
+  return (
+    <div className="text-center my-10">
+      <h1 className="text-2xl">Connections</h1>
+      <div className="flex flex-col gap-2.5 items-center my-5">
+        {connections.map((connection) => {
+          const { firstName, lastName, about, photoUrl, _id, age, gender } =
+            connection;
+          return (
+            <ul
+              key={_id}
+              className="list bg-accent text-white rounded-box shadow-md w-1/2"
+            >
+              <li className="list-row items-center">
+                <div>
+                  <img className="size-10 rounded-2xl" src={photoUrl} />
+                </div>
+                <div>
+                  <div>
+                    <h1 className="font-extrabold">
+                      {firstName + " " + lastName}
+                    </h1>
+                    {age && gender && <p>{age + " " + gender}</p>}
+                  </div>
+                  <div className="text-sm font-bold">{about}</div>
+                </div>
+                <button className="btn btn-square btn-secondary w-full p-2">
+                  Accept
+                </button>
+                <button className="btn btn-square btn-primary w-full p-2">
+                  Reject
+                </button>
+              </li>
+            </ul>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Connections;
