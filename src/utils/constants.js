9b0cce9d8a2842d2ae1c413e6e9aa4c2b36aab96
Muhammed Sudheer A.S <muhammedsudheer948@gmail.com>
2026-08-06 15:52:02 +0530

    made base_url dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,5 @@
-export const BASE_URL = "/api";
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+
+// production use /api
+// local use http://localhost:7777
